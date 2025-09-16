6ff0f4f087d1d1e038823008408af7e0bc98a434
Kaue <Kauecarmo111@gmail.com>
2025-09-16 15:04:08 -0300

    feat: ativando validação dos dados no DTO

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(3000);
