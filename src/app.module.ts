a712bc4d9c1575542bf7645fdb830fb5201b0bb4
Kaue <Kauecarmo111@gmail.com>
2025-09-16 14:20:15 -0300

    chore: exposição da porta pra testes

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: process.env.DB_TYPE as 'mysql',
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT as unknown as number,
+      port: 3306,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
