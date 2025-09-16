78407762b302f8dab9e161f1cac57ab36ad6eae6
Kaue <Kauecarmo111@gmail.com>
2025-09-16 14:20:43 -0300

    fix: corrigindo erro de inicialização, pra que usersModules seja iniciado corretamente junto do auth module

--- src/user/modules/auth/auth.module.ts
+++ src/user/modules/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/user/modules/users/users.module';
@@ -8,7 +8,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    UsersModule,
+    forwardRef(() => UsersModule),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1h' },
