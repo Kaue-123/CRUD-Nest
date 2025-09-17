5d443a4eb9999d5f84f467a7c7c3e884e4924150
Kaue <Kauecarmo111@gmail.com>
2025-09-17 14:25:10 -0300

    fix: ajuste no controller, passando decorators

--- src/infra/send-email/send-email.controller.ts
+++ src/infra/send-email/send-email.controller.ts
@@ -1,11 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { SendEmailService } from './send-email.service';
-import { BodyEmailDto } from 'src/domain/maker/dto/body-email.dto';
+import { BodyEmailDto } from 'src/infra/send-email/dto/body-email.dto';
 
 @Controller('send-email')
 export class SendEmailController {
   constructor(private readonly sendEmailService: SendEmailService) {}
 
+  @HttpCode(HttpStatus.OK)
+  @Post()
   async sendEmail(dto: BodyEmailDto): Promise<any> {
     return this.sendEmailService.sendEmail(dto.to, dto.subject, dto.text);
   }
