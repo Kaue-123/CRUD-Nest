c2337e9b3d5f66049c50b3692dd2533fe450063a
Kaue <Kauecarmo111@gmail.com>
2025-09-19 10:58:43 -0300

    feat: novos testes para lançar erro caso o e-mail não entrou pra fila

--- src/infra/send-email/send-email.controller.spec.ts
+++ src/infra/send-email/send-email.controller.spec.ts
@@ -4,9 +4,12 @@ import { SendEmailController } from './send-email.controller';
 describe('SendEmailController', () => {
   let controller: SendEmailController;
 
+  let emailQueueMock: { add: jest.Mock };
   beforeEach(async () => {
+    emailQueueMock = { add: jest.fn() };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SendEmailController],
+      providers: [{ provide: 'BullQueue_email', useValue: emailQueueMock }],
     }).compile();
 
     controller = module.get<SendEmailController>(SendEmailController);
@@ -14,5 +17,35 @@ describe('SendEmailController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should enqueue an email job and return success', async () => {
+    const emailDto = {
+      to: 'test@example.com',
+      subject: 'Test Subject',
+      text: 'Test Body',
+    };
+    emailQueueMock.add.mockResolvedValueOnce({});
+    // mock getJobCounts
+    controller['emailQueue'].getJobCounts = jest
+      .fn()
+      .mockResolvedValueOnce({ waiting: 1 });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const result = await controller.sendEmail(emailDto);
+    expect(emailQueueMock.add).toHaveBeenCalledWith('send-email', emailDto);
+    expect(result).toEqual({ status: 'Entrou para a fila' });
+  });
+
+  it('should handle error when email does not enter the queue', async () => {
+    const emailDto = {
+      to: 'fail@example.com',
+      subject: 'Fail Subject',
+      text: 'Fail Body',
+    };
+    emailQueueMock.add.mockRejectedValueOnce(new Error('Queue error'));
+    controller['emailQueue'].getJobCounts = jest.fn();
+    await expect(controller.sendEmail(emailDto)).rejects.toThrow(
+      'Error sending email: Queue error',
+    );
   });
 });
