b3d7466ee4b99df2987b218cfbe30e5a690c805e
Kaue <Kauecarmo111@gmail.com>
2025-09-25 16:08:33 -0300

    feat: config inicial do swagger, com a openAPI scalar

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,32 @@ import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('API de Usuários')
+    .setDescription(
+      'API para gerenciamento de usuários com autenticação JWT e envio de e-mails.',
+    )
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  app.use(
+    '/reference',
+    apiReference({
+      content: document,
+      theme: 'purple',
+    }),
+  );
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
