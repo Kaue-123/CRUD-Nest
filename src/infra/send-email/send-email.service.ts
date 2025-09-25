46ed54e49941515175977a493943a3836141a372
Kaue <Kauecarmo111@gmail.com>
2025-09-25 16:08:56 -0300

    feat: passando métrica de incrementação pra envio de e-mail na fila

--- src/infra/send-email/send-email.service.ts
+++ src/infra/send-email/send-email.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectMetric } from '@willsoto/nestjs-prometheus';
 import nodemailer, { Transporter } from 'nodemailer';
+import { Counter } from 'prom-client';
 @Injectable()
 export class SendEmailService {
   private transporter: Transporter;
 
-  constructor() {
+  constructor(
+    @InjectMetric('emails_sent_total')
+    private readonly emailSentCounter: Counter<string>,
+  ) {
     this.transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
@@ -19,6 +24,8 @@ export class SendEmailService {
   async sendEmail(to: string, subject: string, text: string): Promise<any> {
     if (!subject) throw new Error('Subject is required');
     if (!to) throw new Error('Recipient is required');
+
+    this.emailSentCounter.inc();
     return await this.transporter.sendMail({
       from: process.env.EMAIL_USER,
       to,
